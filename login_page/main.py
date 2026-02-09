e7d8c8e8637d71626e8b3e120894b5a2db7a0600
mossabelaktaoui-cell <mossabelaktaoui@gmail.com>
2026-02-09 11:52:30 +0100

    create requirement and .toml file and isolate db connection

--- login_page/main.py
+++ login_page/main.py
@@ -1,10 +1,10 @@
 import sys
-from os import getenv
-from dotenv import load_dotenv
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import Qt
-import mysql.connector
 from login_ui import LoginWindow
+import sys
+sys.path.append('..')
+from db_connection import get_db_connection, close_db_connection
 
 
 def main():
@@ -17,15 +17,9 @@ def main():
 
 
 if __name__ == '__main__':
-    load_dotenv("../.env")
-    connect = mysql.connector.connect(
-        host=getenv("DB_HOST"),
-        user=getenv("DB_USER"),
-        password=getenv("DB_PASSWORD"),
-        database=getenv("DB_NAME")
-    )
-    cursor = connect.cursor()
+    connect, cursor = get_db_connection()
+    
     main()
 
-    cursor.close()
-    connect.close()
+    close_db_connection(connect, cursor)
+    
